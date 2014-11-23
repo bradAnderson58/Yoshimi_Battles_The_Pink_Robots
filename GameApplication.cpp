a048ac2d904083949dd616a7fc6b407bc6d60852
Kevin <kjdec591@gmail.com>
2014-11-23 16:46:25 -0500

    yoshimi collisions
    made robot collisions smoother, and collisions with border walls

--- GameApplication.cpp
+++ GameApplication.cpp
@@ -227,6 +227,18 @@ GameApplication::loadEnv()
 					mNode->setVisible(false);
 					wallList.push_back(mNode);
 				}
+				else if (c == 'b') // create borderwalls
+				{
+					Entity* ent = mSceneMgr->createEntity(getNewName(), Ogre::SceneManager::PT_CUBE);
+					ent->setMaterialName("Examples/RustySteel");
+					Ogre::SceneNode* mNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+					mNode->attachObject(ent);
+					mNode->scale(0.1f,0.2f,0.1f); // cube is 100 x 100
+					grid->getNode(i,j)->setOccupied();  // indicate that agents can't pass through
+					mNode->setPosition(grid->getPosition(i,j).x, 10.0f, grid->getPosition(i,j).z);
+					mNode->setVisible(true);
+					borderWalls.push_back(mNode);
+				}
 				else if (c == 'e')
 				{
 					ParticleSystem::setDefaultNonVisibleUpdateTimeout(5);  // set nonvisible timeout
